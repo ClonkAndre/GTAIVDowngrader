1b1a5b4c6ceb2dc4baaded5947305a3121a46a97
E. S <39125931+ClonkAndre@users.noreply.github.com>
2023-02-19 22:48:52 +0100

    Added FileEncrypter app
    This was mainly used when the Downgrader was still using Google Drive where the files had to be decrypted. This was done using this little tool. Not really necessary anymore, but i will still leave it in.

---
+++ FileEncrypter/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace FileEncrypter {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EncryptFile = new System.Windows.Forms.Button();
+            this.DecryptFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // EncryptFile
+            // 
+            this.EncryptFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.EncryptFile.Location = new System.Drawing.Point(12, 17);
+            this.EncryptFile.Name = "EncryptFile";
+            this.EncryptFile.Size = new System.Drawing.Size(381, 54);
+            this.EncryptFile.TabIndex = 0;
+            this.EncryptFile.Text = "Encrypt file";
+            this.EncryptFile.UseVisualStyleBackColor = true;
+            this.EncryptFile.Click += new System.EventHandler(this.EncryptFile_Click);
+            // 
+            // DecryptFile
+            // 
+            this.DecryptFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DecryptFile.Location = new System.Drawing.Point(12, 78);
+            this.DecryptFile.Name = "DecryptFile";
+            this.DecryptFile.Size = new System.Drawing.Size(381, 54);
+            this.DecryptFile.TabIndex = 1;
+            this.DecryptFile.Text = "Decrypt file";
+            this.DecryptFile.UseVisualStyleBackColor = true;
+            this.DecryptFile.Click += new System.EventHandler(this.DecryptFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(405, 145);
+            this.Controls.Add(this.DecryptFile);
+            this.Controls.Add(this.EncryptFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "File Encrypter for the GTA IV Downgrader";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button EncryptFile;
+        private System.Windows.Forms.Button DecryptFile;
+    }
+}
+
---
+++ FileEncrypter/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Windows.Forms;
+
+namespace FileEncrypter {
+    public partial class Form1 : Form {
+
+        #region Constructor
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        private void EncryptFile_Click(object sender, EventArgs e)
+        {
+            try {
+                using (OpenFileDialog ofd = new OpenFileDialog()) {
+                    if (ofd.ShowDialog() == DialogResult.OK) {
+
+                        string fileName = Path.GetFileName(ofd.FileName);
+                        switch (MessageBox.Show(string.Format("Encrypt file {0}?", fileName), "Confirm Encryption", MessageBoxButtons.YesNo, MessageBoxIcon.Question)) {
+                            case DialogResult.Yes:
+
+                                using (FileStream fs = new FileStream(ofd.FileName, FileMode.Open, FileAccess.Read)) {
+                                    byte[] arr = Helper.GetByteArray(fs);
+                                    File.WriteAllBytes(string.Format(".\\Output\\{0}", fileName), Helper.DataCompression.CompressByteArray(arr, CompressionLevel.Fastest));
+                                }
+
+                                GC.Collect();
+                                MessageBox.Show("File should be encrypted!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                break;
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.ToString(), "Encrypt error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void DecryptFile_Click(object sender, EventArgs e)
+        {
+            try {
+                using (OpenFileDialog ofd = new OpenFileDialog()) {
+                    if (ofd.ShowDialog() == DialogResult.OK) {
+
+                        string fileName = Path.GetFileName(ofd.FileName);
+                        switch (MessageBox.Show(string.Format("Decrypt file {0}?", fileName), "Confirm Decryption", MessageBoxButtons.YesNo, MessageBoxIcon.Question)) {
+                            case DialogResult.Yes:
+
+                                using (FileStream fs = new FileStream(ofd.FileName, FileMode.Open, FileAccess.Read)) {
+                                    byte[] arr = Helper.GetByteArray(fs);
+                                    File.WriteAllBytes(string.Format(".\\Decrypted\\{0}", fileName), Helper.DataCompression.DecompressByteArray(arr));
+                                }
+
+                                GC.Collect();
+                                MessageBox.Show("File should be decrypted!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                break;
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.ToString(), "Decrypt error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+    }
+}
